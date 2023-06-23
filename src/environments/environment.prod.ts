b497fa032b93d30d060cbfa6d0b5297d31da2c72
Deepak <deepakpandeygonda@gmail.com>
2023-06-24 00:30:25 +0700

    Vapid key Add

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   origin: 'https://b8kqfkt2ga.execute-api.ap-southeast-1.amazonaws.com/production',
-  vapidKey: ''
+  vapidKey: 'BPFOxpcO55avaddCVUsKjBp4rHhE-HyzPP-fgEqtYXEI9S6ivVqUunxBocxNzO5pcgpb-HLk2c-wM0pQKWpD_Mo'
 };
